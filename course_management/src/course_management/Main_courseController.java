0bce7ecc3c23ed5b9573888969a731d0859a1376
IMTIAZ MAHAMUD <123954331+IMTIAZ-MAHAMUD@users.noreply.github.com>
2025-07-14 12:55:01 +0600

    3rd review
    main outlook

--- course_management/src/course_management/Main_courseController.java
+++ course_management/src/course_management/Main_courseController.java
@@ -4,9 +4,19 @@
  */
 package course_management;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 /**
  * FXML Controller class
@@ -15,6 +25,19 @@ import javafx.fxml.Initializable;
  */
 public class Main_courseController implements Initializable {
 
+    @FXML
+    private Button home;
+    @FXML
+    private Button profbtn;
+    @FXML
+    private Button ccbtn;
+    @FXML
+    private Button crbtn;
+    @FXML
+    private Button cpbtn;
+    @FXML
+    private Button logoutbtn;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +45,75 @@ public class Main_courseController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void prof(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("profile.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+        
+        Window window = ((Node) event.getSource()).getScene().getWindow();
+        window.hide();
+    }
+
+    @FXML
+    private void cc(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("coursecurriculam.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+        
+        Window window = ((Node) event.getSource()).getScene().getWindow();
+        window.hide();
+    }
+
+    @FXML
+    private void cr(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("courseregistration.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+        
+        Window window = ((Node) event.getSource()).getScene().getWindow();
+        window.hide();
+    }
+
+    @FXML
+    private void cp(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("changepassword.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+        
+        Window window = ((Node) event.getSource()).getScene().getWindow();
+        window.hide();
+    }
+
+    @FXML
+    private void logout(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("Main_course.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+        
+        Window window = ((Node) event.getSource()).getScene().getWindow();
+        window.hide();
+    }
     
 }
